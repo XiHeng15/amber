bea679ce9cfb22c1aa9fdd6022b294b2fde3a4d8
aiden10 <aiden.ten30@gmail.com>
2025-06-21 03:04:11 -0400

    removed sample content

--- amber/app/_layout.tsx
+++ amber/app/_layout.tsx
@@ -1,25 +1,13 @@
-
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
-  return ( 
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
+  return (
+    <>
+      <Stack />
       <StatusBar style="auto" />
-    </Stack>
-
+    </>
   );
 }
